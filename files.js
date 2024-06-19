97214c1bd4a9c960712a30871a48d34f749c1596
kirubasar <kiruba.sri.official@gmail.com>
2024-06-19 12:28:26 +0530

    append file

--- files.js
+++ files.js
@@ -1,8 +1,8 @@
 
 const fs = require('fs');
 
-const message = "Hello, Node.js";
-fs.writeFile('hello.txt', message, (error)=>{
+const message = "Hello, Backhand";
+fs.appendFile('hello.txt', message, (error)=>{
     if(error){
         console.error('Error writing to file');
     } else {
