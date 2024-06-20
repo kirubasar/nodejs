adc68d5318cdeb449f18a9d00f9e6e1b8cb19679
kirubasar <kiruba.sri.official@gmail.com>
2024-06-20 10:36:54 +0530

    create express

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // server.mjs
-const { createServer } =  require('http');
-
-const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!\n');
-});
-
+const express =  require('express');
+const fs = require('fs')
+const app = express();
+app.get('/', (req, res)=>{
+    console.log(req);
+    res.send('hello world')
+})
 // starts a simple http server locally on port 3000
-server.listen(3000, '127.0.0.1', () => {
+app.listen(3000, '127.0.0.1', () => {
   console.log('Listening on 127.0.0.1:3000');
 });
 
